2df16f9de9e7ad0d56e20dd4d16bc04e24b6d056
n1dh1001 <njaga003@ucr.edu>
2024-02-28 12:34:54 -0800

    seeing last edit on note completed

--- include/note.h
+++ include/note.h
@@ -1,24 +1,36 @@
 #ifndef __NOTE_H__
 #define __NOTE_H__
 
+
 #include <string>
+#include <fstream>
+#include <chrono>
+#include <ctime>
 using namespace std;
+
 
 class Note {
     private:
         string title;
         string body;
-        string lastEdit; // date - time (03-06-2024 - 12:36:23)
+        string lastEdit;
+        //time_zone lastEdit; // date - time (03-06-2024 - 12:36:23)
+
 
     public:
         Note();
         Note(string titleName, string body, string lastEdit);
         ~Note();
 
-        string getTitle(); 
-        string getBody(); 
+
+        string getTitle();
+        string getBody();
         string getLastEdit();
-        void renameTitle();
+        void setLastEdit();
+        void renameTitle(const string& newName);
+        int outputNote();
+        int writeNoteToFile();
 };
 
-#endif 
+
+#endif
